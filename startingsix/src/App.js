58612b503e67351a13a0a87b339f05ddef2dd35a
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-11 02:58:37 -0500

    add colors

--- startingsix/src/App.js
+++ startingsix/src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 function App() {
 	return (
 		<div className="App">
-			<p className="text-Mysterious">qwidhfaskjdhfksjadhfksjd</p>
+			<p className="text-formalAffairs">qwidhfaskjdhfksjadhfksjd</p>
 		</div>
 	);
 }
--- startingsix/tailwind.config.js
+++ startingsix/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				mysterious: "#070724",
+				carbon: "#38384F",
+				formalAffairs: "#838391",
+				fairyTaleBlue: "#419EBB",
+				goldenKoi: "#EDA249",
+				kaidoLavender: "#6f2ed6",
+				charmanderRed: "#D14C32",
+				charmeleonRed: "#D83A34",
+				rustyNail: "#CD5120",
+				tealish: "#1ec2a4",
+				retroBlue: "#2d68f0",
+			},
+		},
 	},
 	plugins: [],
 };
