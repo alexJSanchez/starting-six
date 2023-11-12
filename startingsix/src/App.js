815c66e6e84b769641fee28d7e3aa9984b8391de
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-12 04:50:13 -0500

    nav complete no hamburger

--- startingsix/src/App.js
+++ startingsix/src/App.js
@@ -2,9 +2,9 @@ import logo from "./assets/icon-hamburger.svg";
 
 function App() {
 	return (
-		<div className="App bg-[#070724]">
-			<nav className="flex justify-between items-center">
-				<p className="header-two">The Planets</p>
+		<div className="App bg-[#000000] bg-contain bg-no-repeat">
+			<nav className="flex justify-between items-center px-[1.5rem] py-4">
+				<p className="header-two text-[1.5rem]">The Planets</p>
 				<div>
 					<img src={logo}></img>
 				</div>
