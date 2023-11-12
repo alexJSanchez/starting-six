2ae78c4af8d6259270653f8eb3fc634a817d990b
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-12 04:56:29 -0500

    add border line

--- startingsix/src/App.js
+++ startingsix/src/App.js
@@ -2,13 +2,14 @@ import logo from "./assets/icon-hamburger.svg";
 
 function App() {
 	return (
-		<div className="App bg-[#000000] bg-contain bg-no-repeat">
-			<nav className="flex justify-between items-center px-[1.5rem] py-4">
+		<div className="App bg-[#000000] ">
+			<nav className="flex justify-between items-center px-[1.5rem] py-4 border-b-[1px] border-opacity-[0.2] border-white">
 				<p className="header-two text-[1.5rem]">The Planets</p>
 				<div>
 					<img src={logo}></img>
 				</div>
 			</nav>
+			<main className="bg-black ">hello my friend</main>
 		</div>
 	);
 }
