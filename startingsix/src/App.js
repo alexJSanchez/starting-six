3b42d03a671b498d3f12ed00dbe1e34af7fe74f9
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-12 20:35:48 -0500

    stable static homepage

--- startingsix/src/App.js
+++ startingsix/src/App.js
@@ -1,33 +1,67 @@
 import logo from "./assets/icon-hamburger.svg";
+import planet from "./assets/planet-mercury.svg";
 
 function App() {
 	return (
-		<div className="App bg-mysterious ">
+		<div className="App bg-mysterious pb-12">
 			<nav className="flex justify-between items-center px-[1.5rem] py-4 border-b-[1px] border-opacity-[0.2] border-white">
 				<p className="header-two text-[1.5rem]">The Planets</p>
 				<div>
 					<img src={logo}></img>
 				</div>
 			</nav>
-			<main className=" bg-mysterious text-white">
-				<div className="grid">
-					<button className="flex pl-7 py-3 border-[1px] border-opacity-[0.2] border-white">
+			<main className="bg-mysterious text-white">
+				{/* top */}
+				<div className="grid grid-cols-3">
+					<button className="btn flex items-center justify-center border-[1px] border-opacity-[0.2] border-white ">
 						<p>01</p>button
 					</button>
-					<button className="flex pl-7 py-3 border-[1px] border-opacity-[0.2] border-white">
+					<button className="btn flex items-center justify-center border-[1px] border-opacity-[0.2] border-white">
 						<p>02</p>button
 					</button>
-					<button className="flex pl-7 py-3 border-[1px] border-opacity-[0.2] border-white">
+					<button className="btn flex items-center justify-center border-[1px] border-opacity-[0.2] border-white">
 						<p>03</p>button
 					</button>
-					<button className="flex pl-7 py-3 border-[1px] border-opacity-[0.2] border-white">
+					<button className="btn flex items-center justify-center border-[1px] border-opacity-[0.2] border-white">
 						<p>04</p>button
 					</button>
-					<button className="flex pl-7 py-3 border-[1px] border-opacity-[0.2] border-white">
+					<button className="btn flex items-center justify-center border-[1px] border-opacity-[0.2] border-white">
 						<p>05</p>button
 					</button>
-					<button className="flex pl-7 py-3 border-[1px] border-opacity-[0.2] border-white">
+					<button className="btn flex items-center justify-center border-[1px] border-opacity-[0.2] border-white">
 						<p>06</p>button
+					</button>
+				</div>
+				{/* middle */}
+				<div className="flex flex-col items-center justify-center">
+					<img src={planet} className="px-[132px] py-24" />
+					<h2 className="header-two">mercury</h2>
+					<p className="description px-9 pt-4 pb-8">
+						Mercury is the smallest planet in the Solar System and the closest
+						to the Sun. Its orbit around the Sun takes 87.97 Earth days, the
+						shortest of all the Sun's planets. Mercury is one of four
+						terrestrial planets in the Solar System, and is a rocky body like
+						Earth.
+					</p>
+				</div>
+				{/*bottom*/}
+				<div className="grid gap-4 px-6">
+					<button className="flex border opacity-50 items-center px-6 py-4 justify-between">
+						<p className="body-two">ROTATION TIME</p>
+						<p>58.6 days</p>
+					</button>
+
+					<button className="flex border opacity-50 items-center px-6 py-4 justify-between">
+						<p className="body-two">ROTATION TIME</p>
+						<p>58.6 days</p>
+					</button>
+					<button className="flex border opacity-50 items-center px-6 py-4 justify-between">
+						<p className="body-two">ROTATION TIME</p>
+						<p>58.6 days</p>
+					</button>
+					<button className="flex border opacity-50 items-center px-6 py-4 justify-between">
+						<p className="body-two">ROTATION TIME</p>
+						<p>58.6 days</p>
 					</button>
 				</div>
 			</main>
